74f33ee61e9025d67dce20099f73bee4384c301f
Claudio Clemens <Claudio.Clemens@lv1871.de>
2022-11-29 18:44:01 +0100

    Test 3 - Grün
    - baby step

--- src/main/java/de/lv1871/dojos/roman/RomanNumeral.java
+++ src/main/java/de/lv1871/dojos/roman/RomanNumeral.java
@@ -3,8 +3,8 @@ package de.lv1871.dojos.roman;
 public class RomanNumeral {
 
 	public String convert(int naturalNumber) {
-		if (naturalNumber == 2) {
-			return "II";
+		if (naturalNumber > 1) {
+			return "I" + convert(naturalNumber - 1);
 		}
 		return "I";
 	}
