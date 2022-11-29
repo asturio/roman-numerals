8cbdab2c0c99ca858e9aa00462de44421e6e8273
Claudio Clemens <Claudio.Clemens@lv1871.de>
2022-11-29 18:35:56 +0100

    Test 2 - Rot
    - Assertion 2 = II

--- src/test/java/de/lv1871/dojos/roman/RomanNumeralTest.java
+++ src/test/java/de/lv1871/dojos/roman/RomanNumeralTest.java
@@ -6,12 +6,22 @@ import org.junit.jupiter.api.Test;
 public class RomanNumeralTest {
 
 	@Test
-	void romanOf1_shouldBeI() {
+	void romanOf1_shouldBe_I() {
 		// given
 		RomanNumeral romanNumeral = new RomanNumeral();
 		// when
 		String roman = romanNumeral.convert(1);
 		// then
 		Assertions.assertThat(roman).isEqualTo("I");
+	}
+
+	@Test
+	void romanOf2_shouldBe_II() {
+		// given
+		RomanNumeral romanNumeral = new RomanNumeral();
+		// when
+		String roman = romanNumeral.convert(2);
+		// then
+		Assertions.assertThat(roman).isEqualTo("II");
 	}
 }
