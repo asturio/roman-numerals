61eda3bfab9ea5db211d73266516f9e10f310c32
Claudio Clemens <Claudio.Clemens@lv1871.de>
2022-11-29 18:09:46 +0100

    Initial Commit - Test 1
    - Konstruktor

---
+++ src/test/java/de/lv1871/dojos/roman/RomanNumeralTest.java
@@ -0,0 +1,13 @@
+package de.lv1871.dojos.roman;
+
+import org.junit.jupiter.api.Test;
+
+public class RomanNumeralTest {
+	@Test
+	void test() {
+		// given
+		RomanNumeral romanNumeral = new RomanNumeral();
+		// when
+		// then
+	}
+}
