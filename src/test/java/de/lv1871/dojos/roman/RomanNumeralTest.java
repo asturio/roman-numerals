85790683dbb4ede75950b4f6e32afca812128ce1
Claudio Clemens <Claudio.Clemens@lv1871.de>
2022-11-29 18:40:44 +0100

    Tests refactored

--- src/test/java/de/lv1871/dojos/roman/RomanNumeralTest.java
+++ src/test/java/de/lv1871/dojos/roman/RomanNumeralTest.java
@@ -1,27 +1,21 @@
 package de.lv1871.dojos.roman;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class RomanNumeralTest {
 
+	private RomanNumeral romanNumeral = new RomanNumeral();
+
 	@Test
 	void romanOf1_shouldBe_I() {
-		// given
-		RomanNumeral romanNumeral = new RomanNumeral();
-		// when
-		String roman = romanNumeral.convert(1);
-		// then
-		Assertions.assertThat(roman).isEqualTo("I");
+		assertThat(romanNumeral.convert(1)).isEqualTo("I");
 	}
 
 	@Test
 	void romanOf2_shouldBe_II() {
-		// given
-		RomanNumeral romanNumeral = new RomanNumeral();
-		// when
-		String roman = romanNumeral.convert(2);
-		// then
-		Assertions.assertThat(roman).isEqualTo("II");
+		assertThat(romanNumeral.convert(2)).isEqualTo("II");
 	}
 }
