12bbf4b50ee7e9681b2482a1160eb74edf31a440
Claudio Clemens <Claudio.Clemens@lv1871.de>
2022-11-29 18:18:45 +0100

    Test 1 - Rot
    - Assertion hinzugefügt: 1 -> I

--- src/test/java/de/lv1871/dojos/roman/RomanNumeralTest.java
+++ src/test/java/de/lv1871/dojos/roman/RomanNumeralTest.java
@@ -1,8 +1,10 @@
 package de.lv1871.dojos.roman;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class RomanNumeralTest {
+
 	@Test
 	void romanOf1_shouldBeI() {
 		// given
@@ -10,6 +12,6 @@ public class RomanNumeralTest {
 		// when
 		String roman = romanNumeral.convert(1);
 		// then
-
+		Assertions.assertThat(roman).isEqualTo("I");
 	}
 }
